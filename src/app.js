661c5565fa7973cc0de7262f6326f0e4fc702f59
Ganesh Maske <ganeshmaske7447@gmail.com>
2026-03-26 00:35:26 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,22 @@
+ const express = require("express");
+
+ const app = express();
+//  app.use( "/",(req,res,)=>{
+//     res.send("Namaste Ganesh!");
+//  });
+
+ app.use( "/hello",(req,res)=>{
+    res.send("Hello Hello Hello!");
+ });
+
+ app.use("/test",(req,res)=>{
+    res.send("Hello from the server!");
+ });
+
+  app.use( "/",(req,res)=>{
+    res.send("Namaste Ganesh!");
+ });
+
+ app.listen(3000,()=>{
+    console.log("Server is Successfully listeing on port 3000...");
+ });
