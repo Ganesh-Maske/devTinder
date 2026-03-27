80886a934c2f63c32c29f07393597b2fd0af4806
Ganesh Maske <ganeshmaske7447@gmail.com>
2026-03-27 15:23:49 +0530

    exploring get ,post,delete

--- src/app.js
+++ src/app.js
@@ -1,21 +1,33 @@
  const express = require("express");
 
  const app = express();
-//  app.use( "/",(req,res,)=>{
-//     res.send("Namaste Ganesh!");
-//  });
-
- app.use( "/hello",(req,res)=>{
-    res.send("Hello Hello Hello!");
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Ganesh", lastName: "Maske"});
  });
 
+ app.post("/user",(req,res)=>{
+   console.log(req.body);
+   //saving data to DB
+    res.send("Data successsfully saved to the database!");
+ });
+
+ app.delete("/user",(req,res)=>{
+    res.send("Deleted successsfully!");
+ });
+
+
+// this will match all the HTTP method API calls to 
  app.use("/test",(req,res)=>{
     res.send("Hello from the server!");
  });
 
-  app.use( "/",(req,res)=>{
-    res.send("Namaste Ganesh!");
- });
+ //  app.use( "/hello",(req,res)=>{
+//     res.send("Hello Hello Hello!");
+//  });
+
+//   app.use( "/",(req,res)=>{
+//     res.send("Namaste Ganesh!");
+//  });
 
  app.listen(3000,()=>{
     console.log("Server is Successfully listeing on port 3000...");
