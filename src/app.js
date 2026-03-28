5625186526b8dccf2ccad1e1d2faa2c6bbc21f74
Ganesh Maske <ganeshmaske7447@gmail.com>
2026-03-28 20:13:32 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -34,6 +34,32 @@ app.get("/user",(req,res)=>{
 //     res.send("Namaste Ganesh!");
 //  });
 
+
+//("/route",rH,[rH2,rH3],rH4,rH5);
+app.use("/user",(req,res,next)=>{
+   console.log("Handling the route user!!");
+   next();
+},
+  (req,res,next)=>{
+   console.log("Handling the route user 2!!");
+   //res.send("2nd Response!!");
+   next();
+ },
+  (req,res,next)=>{
+   console.log("Handling the route user 3!!");
+   //res.send("3rd Response!!");
+   next();
+ },
+  (req,res,next)=>{
+   console.log("Handling the route user 4!!");
+   //res.send("4th Response!!");
+   next();
+ }, (req,res,next)=>{
+   console.log("Handling the route user 5!!");
+   res.send("5th Response!!");
+   next();
+ });
+
  app.listen(3000,()=>{
     console.log("Server is Successfully listeing on port 3000...");
  });
