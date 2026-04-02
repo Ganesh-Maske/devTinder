95028507a9a15f6ceac974822c83d2580242fd3d
Ganesh Maske <ganeshmaske7447@gmail.com>
2026-04-02 19:08:43 +0530

    create API s Delete and Update

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./model/user");  
 
+
 const app = express();
 app.use(express.json());
-
+/////
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
   try {
@@ -14,7 +15,68 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("Error saving the user: " + err.message);
   }
 });
+  
+/////
+app.get("/user", async (req,res)=>{
+  const useremail = req.body.emailId;
+  // try{
+  //  const user = await User.findOne({ emailId: useremail });
+  //   if(!user){
+  //     res.status(404).send("user not found");
+  //   }else{
+  //     res.send(user);
+  //   }
 
+    try{
+   const users = await User.find({ emailId: useremail });
+    if(users.length === 0){
+      res.status(404).send("user not found");
+    }else{
+      res.send(users);
+    }
+
+  }catch(err){
+    res.status(400).send("somthing went wrong");
+  }
+});
+
+//////
+app.get("/feed",async(req,res)=>{
+ try{
+  const users = await User.find({})
+  res.send(users)
+ }catch(err){
+  res.status(400).send("something went wrong");
+ }
+});
+
+/////
+app.delete("/user", async(req,res)=>{
+  const userId =req.body.userId;
+  try{
+    //const User =await User .findByIdAndDelete({_id:userId});
+    const user = await User .findByIdAndDelete(userId);
+    res.send("user deleted Successfully");
+  }catch(err){
+    res.status(400).send("something went wrong");
+  }
+});
+
+/////
+
+app.patch("/user", async(req,res)=>{
+  const userId =req.body.userId;
+  const data = req.body;
+  try{
+    const user = await User .findByIdAndUpdate({_id:userId},data,{rteunDocument:"after", 
+
+    });
+    console.log(user);
+    res.send("user deleted Successfully");
+  }catch(err){
+    res.status(400).send("something went wrong");
+  }
+});
 connectDB()
   .then(() => {
     console.log("Database connection established....");
