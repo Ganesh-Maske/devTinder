b2cae66c9d7d8b6e322c7eca58ed19f16b5872e6
Ganesh Maske <ganeshmaske7447@gmail.com>
2026-04-24 01:20:58 +0530

    create connection and feed APIs

---
+++ src/routes/user.js
@@ -0,0 +1,92 @@
+const express = require("express");  
+const userRouter = express.Router();
+const userAuth = require("../middlewares/auth");
+const ConnectionRequestModel =require("../model/connectionRequest");  
+const User = require ("../model/user");
+const user = require("../model/user");
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+//Get all the pending connection request for the loggedIn user
+
+userRouter.get("/user/request/received",userAuth, async(req,res)=>{
+    try{
+        const loggedInUser =req.user;
+        const connectionRequest =await ConnectionRequestModel.find({
+            toUserId:loggedInUser._id,
+            status :"interested",
+        }).populate ("fromUserId", USER_SAFE_DATA);
+        //}).populate("fromUserId", ["firstName","lastName"]);
+
+        res.json({
+            message:"Data fetched successfully",
+            Data: connectionRequest,
+        })
+    }catch(err){
+        res.status(400).send("Error" + err.message);
+    }
+});
+
+
+userRouter.get("/user/connections", userAuth,async(req,res)=>{
+    try{
+       const loggedInUser = req.user; 
+        const connectionRequest = await ConnectionRequestModel.find({
+            $or:[
+                {toUserId:loggedInUser._id, status:"accepted"},
+                {fromUserId:loggedInUser._id,status:"accepted"},
+            ],
+        }).populate("fromUserId",USER_SAFE_DATA)
+          .populate("toUserId",USER_SAFE_DATA);
+
+          console.log(connectionRequest);
+
+          const data =connectionRequest.map((row)=>{
+            if(row.fromUserId._id.toString()===loggedInUser._id.toString()){
+                return row.toUserId;
+            }
+            return row.fromUserId;
+          });
+     res.json({data});
+    }catch(err){
+        res.status(400).send({message:err.message});
+    }
+});
+
+
+userRouter.get("/feed", userAuth,async(req,res)=>{
+    try{
+        const loggedInUser =req.user;
+        
+        const page = parseInt(req.query.page)|| 1;
+        let limit = parseInt(req.query.limit)|| 10;
+        limit = limit >50 ? 50 :limit;
+        const skip =(page-1)*limit;
+
+
+        const connectionRequest =await ConnectionRequestModel.find({
+            $or:[{fromUserId:loggedInUser._id},{toUserId:loggedInUser._id}],
+        }).select("fromUserId toUserId");
+
+        const hiddenUsersFromFeed = new Set();
+        connectionRequest.forEach((row)=>{
+            hiddenUsersFromFeed.add(row.fromUserId.toString());
+            hiddenUsersFromFeed.add(row.toUserId.toString());
+        });
+        console.log(hiddenUsersFromFeed);
+
+        const users = await User.find({
+            $and:[
+                {_id: {$nin:Array.from(hiddenUsersFromFeed)}},
+                {_id:{$ne:loggedInUser._id}},
+            ],
+        }).select(USER_SAFE_DATA)
+        .skip(skip)
+        .limit(limit);
+        
+        res.json({data:users});
+
+    }catch(err){
+        res.status(400).json({message:err.message})
+    }
+});
+
+module.exports = userRouter;
